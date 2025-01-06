ff58b4d8908361b7250c5cafaf5f6d9fc31cad03
MarkUA <mark.d.nahorniak@gmail.com>
2025-01-06 19:06:27 +0200

    fix

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const users = [
-    { id: "user-1", name: "Tima", email: "tima@example.com", age: 15, online: true },
-    { id: "user-2", name: "flightfusion", email: "flightfusion@example.com", age: 12, online: true },
-    { id: "user-3", name: "craftMUA", email: "craftMUA@example.com", age: 10, online: false },
-    { id: "user-4", name: "Dima", email: "dima@example.com", age: 13, online: false },
-    { id: "user-5", name: "Ajax", email: "ajax@example.com", age: 43, online: true },
-  ];
+  { id: "user-1", name: "Tima", email: "tima@example.com", age: 15, eyeColor: "brown", isActive: true, gender: "male" },
+  { id: "user-2", name: "flightfusion", email: "flightfusion@example.com", age: 12, eyeColor: "brown", isActive: false, gender: "female" },
+  { id: "user-3", name: "craftMUA", email: "craftMUA@example.com", age: 10, eyeColor: "blue", isActive: true, gender: "male" },
+  { id: "user-4", name: "Dima", email: "dima@example.com", age: 13, eyeColor: "brown", isActive: false, gender: "male" },
+  { id: "user-5", name: "Ajax", email: "ajax@example.com", age: 43, eyeColor: "blue", isActive: true, gender: "female" }
+]
+
   
   // Task 1
   const userNames = users.map((user) => user.name);
   console.log(userNames);
-  
+
+  // Task 2
+  const filterEye = users.filter(user => user.eyeColor === "brown")
+  console.log(filterEye)
+
+  // Task 3
+  const filterGender = users.filter(user => user.gender === "female")
+  console.log(filterGender)
+
+  // Task 4
+  const filterActive = users.filter(user => user.isActive === false)
+  console.log(filterActive)
+
   // Task 5
   const findUserByEmail = (email) => {
     let user = null;
